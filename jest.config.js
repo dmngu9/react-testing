6911e1bc69c33bfafea174396ac57fd1febbeecf
dmngu9 <minh.d.nguyen2211@gmail.com>
2021-08-01 05:18:56 +0000

    Updates

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  verbose: true,
-  bail: true,
-  roots: ['<rootDir>/src'],
-  moduleDirectories: ['node_modules', 'src'],
-  moduleFileExtensions: ['js', 'ts', 'tsx'],
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
-};
--- src/utils/getUser.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const getUser = async () => {
-  const { data } = await axios.get('https://api.github.com/users/dmngu9')
-  return data
-}
-
-export { getUser }
